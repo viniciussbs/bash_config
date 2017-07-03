ffdf5f291171993903a2b3c81a2e8ef3e6ec73cf
Vinícius Sales <vinicius.sbs@gmail.com>
2017-07-03 12:31:05 -0300

    New prompt style

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -1,16 +1,24 @@
 __wesolve_prompt () {
   # Colors
-  local BLUE="\[\033[0;34m\]"
-  local NO_COLOR="\[\e[0m\]"
-  local GRAY="\[\033[1;30m\]"
-  local GREEN="\[\033[0;32m\]"
-  local LIGHT_GRAY="\[\033[0;37m\]"
-  local LIGHT_GREEN="\[\033[1;32m\]"
-  local LIGHT_RED="\[\033[1;31m\]"
+  local BLACK="\[\033[0;30m\]"
   local RED="\[\033[0;31m\]"
-  local WHITE="\[\033[1;37m\]"
-  local YELLOW="\[\033[0;33m\]"
+  local GREEN="\[\033[0;32m\]"
+  local ORANGE="\[\033[0;33m\]"
+  local BLUE="\[\033[0;34m\]"
   local PURPLE="\[\033[0;35m\]"
+  local CYAN="\[\033[0;36m\]"
+  local LIGHT_GREY="\[\033[1;37m\]"
+  # Bright Colors
+  local DARK_GREY="\[\033[1;30m\]"
+  local LIGHT_RED="\[\033[0;31m\]"
+  local LIGHT_GREEN="\[\033[1;32m\]"
+  local YELLOW="\[\033[1;33m\]"
+  local LIGHT_BLUE="\[\033[1;34m\]"
+  local LIGHT_PURPLE="\[\033[1;35m\]"
+  local LIGHT_CYAN="\[\033[1;36m\]"
+  local WHITE="\[\033[1;37m\]"
+  # Default Colors
+  local NO_COLOR="\[\e[0m\]"
   local BASE_COLOR="$NO_COLOR"
 
   # Branch
@@ -65,12 +73,16 @@ __wesolve_prompt () {
     fi
 
     if [[ "$STATUS" =~ "$UNTRACKED" ]]; then
-      STATE="${STATE}${YELLOW}*${NO_COLOR}"
+      STATE="${STATE}${ORANGE}*${NO_COLOR}"
     fi
 
-    PS1="\n${BASE_COLOR}┌ \w\a${NO_COLOR} ${PROMPT_COLOR}${BRANCH}${NO_COLOR}${STATE}${NO_COLOR}\n└⟩"
+    PS1="
+${LIGHT_CYAN}│ ${BASE_COLOR}\w\a${NO_COLOR} ${PROMPT_COLOR}${BRANCH}${NO_COLOR}${STATE}
+${LIGHT_CYAN}│ ${NO_COLOR}\$ "
   else
-    PS1="\n${BASE_COLOR}┌ \w\a${NO_COLOR}\n└⟩"
+    PS1="
+${LIGHT_CYAN}│ ${BASE_COLOR}\w\a
+${LIGHT_CYAN}│ ${NO_COLOR}\$ "
   fi
 }
 
