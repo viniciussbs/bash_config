86bb043fa9493205cb6e8a39bc9bcb0804345007
Vinícius <vinicius.sbs@gmail.com>
2018-07-28 17:47:36 -0300

    Some utilities created a long time ago - I don't remember why

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -95,3 +95,21 @@ complete -F _repo repo
 see() {
   qlmanage -p "$@" &>/dev/null
 }
+
+latest() {
+    if [[ "$1" != '' ]]; then
+        echo "$1$(ls -rt $1 | tail -n1)"
+    else
+        echo "$(ls -rt | tail -n1)"
+    fi
+}
+
+abs_path() {
+    if [[ "$1" != '' ]]; then
+        echo "$(pwd)/$1"
+    else
+        local stdin
+        stdin=$(cat)
+        echo "$(pwd)/$stdin"
+    fi
+}
